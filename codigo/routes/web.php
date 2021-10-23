59cf7117d91b63210cdc939fb33c412c6291680e
Larissa <81172873+larisprado@users.noreply.github.com>
2021-10-23 19:43:09 -0300

    Add files via upload

--- codigo/routes/web.php
+++ codigo/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clientes', [ClienteController::class, 'show']);
+
+Route::get('/fornecedor', [FornecedorController::class, 'show']);
