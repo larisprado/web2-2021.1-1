09ca6100bea9899218c430d489eb51df6a169db8
Larissa <81172873+larisprado@users.noreply.github.com>
2021-10-27 10:27:23 -0300

    Add files via upload

--- codigo/routes/web.php
+++ codigo/routes/web.php
@@ -19,6 +19,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/clientes', [ClienteController::class, 'show']);
+// Route::get('/clientes', [ClienteController::class, 'show']);
 
-Route::get('/fornecedor', [FornecedorController::class, 'show']);
+// Route::get('/fornecedor', [FornecedorController::class, 'show']);
+
+// Route::get('/', [FornecedorController::class, 'index']);
+// Route::get('/create', [FornecedorController::class, 'create']);
+// Route::post('/store', [FornecedorController::class, 'store']);
+
+Route::prefix('clientes')->group(function () {
+    Route::get('/show', [ClienteController::class, 'show']);
+
+    Route::get('/', [ClienteController::class, 'index']);
+    Route::get('/create', [ClienteController::class, 'create']);
+    Route::post('/store', [ClienteController::class, 'store']);
+});
+
+Route::prefix('fornecedor')->group(function () {
+    Route::get('/show', [FornecedorController::class, 'show']);
+
+    Route::get('/', [FornecedorController::class, 'index']);
+    Route::get('/create', [FornecedorController::class, 'create']);
+    Route::post('/store', [FornecedorController::class, 'store']);
+});
