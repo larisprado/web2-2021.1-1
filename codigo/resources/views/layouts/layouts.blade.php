4bc89779274d9921a82b4eb7df3f0f86a0397407
Larissa <81172873+larisprado@users.noreply.github.com>
2021-12-22 07:52:21 -0300

    Add files via upload

--- codigo/resources/views/layouts/layouts.blade.php
+++ codigo/resources/views/layouts/layouts.blade.php
@@ -18,7 +18,8 @@
 
 </head>
 
-<body>
+<body >
+  <div id="layout">
     <div id="container">
                     <div class="title">
 
@@ -36,6 +37,7 @@
             copyright @WEBII Larissa-Prado
         </div>
     </div>
+  </div>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
